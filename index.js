c8ab21c7dc17a44456a7e4d43fc4b9da75363eb8
Kevin Martensson <kevinmartensson@gmail.com>
2017-05-23 18:21:49 +0200

    Check if fork is in use before removing it
    Fixes #2.

--- index.js
+++ index.js
@@ -1,9 +1,42 @@
 'use strict';
 const ghGot = require('gh-got');
+const githubBranches = require('github-branches');
 const githubRepos = require('github-repositories');
 const githubTokenUser = require('github-token-user');
+const pFilter = require('p-filter');
+const pLocate = require('p-locate');
 
+const getDiff = (repo, base, head, token) => ghGot(`repos/${repo.parent.full_name}/compare/${base}...${head}`, {token}).then(res => res.body);
+const getRepo = (repo, token) => ghGot(`repos/${repo.full_name}`, {token}).then(res => res.body);
 const deleteRepo = (repo, token) => ghGot.delete(`repos/${repo.full_name}`, {token}).then(() => repo);
+
+const isBranchSame = (repo, branch, parentBranches, token) => {
+	const sha = parentBranches.filter(x => x.commit.sha === branch.commit.sha);
+	const checkBranches = parentBranches.filter(x => [branch.name, 'master'].some(y => y === x.name));
+
+	if (sha.length === 1) {
+		return true;
+	}
+
+	return pLocate(checkBranches, x => getDiff(repo, branch.commit.sha, x.name, token)
+		.then(res => res.behind_by === 0)
+		.catch(err => {
+			if (err.code === 404) {
+				return true;
+			}
+
+			throw err;
+		}));
+};
+
+const isBranchNotUseful = (repo, token) => githubBranches(repo.full_name, {token})
+	.then(branches => Promise.all([branches, githubBranches(repo.parent.full_name, {token})]))
+	.then(data => {
+		const branches = data[0];
+		const parentBranches = data[1];
+
+		return branches.length <= parentBranches.length && pLocate(branches, x => isBranchSame(repo, x, parentBranches, token));
+	});
 
 module.exports = opts => {
 	opts = opts || {};
@@ -15,5 +48,7 @@ module.exports = opts => {
 	return githubTokenUser(opts.token)
 		.then(data => githubRepos(data.login, {token: opts.token}))
 		.then(data => data.filter(x => x.fork))
+		.then(data => Promise.all(data.map(x => getRepo(x, opts.token))))
+		.then(data => pFilter(data, x => isBranchNotUseful(x, opts.token)))
 		.then(data => Promise.all(data.map(x => deleteRepo(x, opts.token))));
 };
